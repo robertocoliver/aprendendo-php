dc3a26d5d45c9cb9c853bd27be81fae3f0f6fa15
Roberto Oliveira <robertocoliver@protonmail.com>
2023-07-05 02:19:04 -0300

    Create ex000.php

---
+++ cursoemvideo/cursophp/ex000/ex000.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro código</title>
+</head>
+
+<body>
+   <h1>
+        <?php 
+            echo "Meu primeiro código em PHP \u{1F642}"; 
+        ?>
+   </h1>   
+</body>
+</html>
+
