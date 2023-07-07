e0925cdaececab6a50588b962ede627d64cffb59
root <root@kali>
2023-07-07 20:18:02 -0300

    const

---
+++ cursophp/ex004/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do servidor</title>
+</head>
+<body>
+    <?php
+        $nome = "joão"; 
+        const ESTADO = "São Paulo"; 
+        echo "Muito prazer $nome! Você mora no estado de " . ESTADO;
+    ?>    
+</body>
+</html>
