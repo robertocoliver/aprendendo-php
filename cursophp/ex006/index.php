50cb557d399b5f928fc76b6cb7efba7028d84180
root <root@kali>
2023-07-08 13:38:10 -0300

    Tipo primitivo composto array

---
+++ cursophp/ex006/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de tipos primitivos</title>
+</head>
+<body>
+    <?php
+        $v = [1, 1.5, "string", true];
+        // echo "$v";
+        var_dump($v);       
+    ?>
+</body>
+</html>
