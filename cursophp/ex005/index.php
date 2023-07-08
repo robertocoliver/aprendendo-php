1876af4620ab93aaba8e63f2310f7adbfc2a0ddb
root <root@kali>
2023-07-08 12:59:41 -0300

    Tipos primitivos

---
+++ cursophp/ex005/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos</title>
+</head>
+<body>
+    
+    <?php
+        $num = 0x1A; 
+        echo "O valor da váriavel é $num e seu tipo primitivo é ";
+        
+        var_dump($num)
+        
+    ?>
+</body>
+</html>
+
