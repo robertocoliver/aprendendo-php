aad1d980933016d302fff3e60e91935a6922474c
root <root@kali>
2023-07-09 01:21:23 -0300

    Concatenando 2 strings

---
+++ manipulação de string/ex009/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $teste  = "Olá";
+        $teste1 = "Roberto";
+        echo "$teste" . "$teste1";
+    ?>
+</body>
+</html>
