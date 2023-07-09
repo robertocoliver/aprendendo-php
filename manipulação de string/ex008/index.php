22cc6b578b41985771834e637ea4fd4228416d6e
root <root@kali>
2023-07-09 00:06:14 -0300

    double squoted and single quoted

---
+++ manipulação de string/ex008/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulação de Strings</title>
+</head>
+<body>
+    <?php 
+        // echo Ola \u{1F418}
+        echo 'Ola \u{1F418}';
+    ?>
+</body>
+</html>
