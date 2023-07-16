388782d364aa1e54843316b23d142244e4027aea
root <root@kali>
2023-07-16 01:05:37 -0300

    usando formatação de números + comentários

--- exercicios/ex04/cad.php
+++ exercicios/ex04/cad.php
@@ -14,9 +14,9 @@
             // Resolução: echo " Seus R\$ " . number_format($real, 2, ",", "."). " equivalem a US\$ " . number_format($dolar,2, ",", ".");       
 
             $real = $_POST["real"];
-            $url = "https://economia.awesomeapi.com.br/last/USD";
-            $api = file_get_contents($url);
-            $json = json_decode($api, true);
+            $url = "https://economia.awesomeapi.com.br/last/USD"; // A URL não possui cifrão, por isso não há necessidade de aspas simples para o interpretador não procurar por uma variável e fazer o scape. 
+            $req = file_get_contents($url);
+            $json = json_decode($req, true);
             $string = $json['USDBRL']['bid'];
             $dolar = floatval($string);
 
